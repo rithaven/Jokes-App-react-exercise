ca6993b2fd2e280e848c01026e036917261b0470
rithaven <veneruth4@gmail.com>
2023-04-11 12:50:08 +0200

    Removed unnecessary spaces and namings

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import RenderJokes from './component/RenderJokes';
 import './index.css';
 
 function App() {
-
   return (
     <div className='flex flex-col items-center justify-center h-screen bg-[#e2f7db]'>
       <RenderJokes/>
     </div>
   );
 }
-
 export default App;
--- src/component/RenderJokes.jsx
+++ src/component/RenderJokes.jsx
@@ -1,5 +1,3 @@
-
-
 const RenderJokes = () => {
     const jokes = [
         {
